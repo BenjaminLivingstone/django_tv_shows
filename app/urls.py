857c602f1f791640161eb595a887785a6f8c9dcc
BenjaminLivingstone <benjaminlivingstone@gmail.com>
2021-08-20 20:45:38 -0400

    Edit Form Missing

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('shows/new/', views.newshowform),
     path('shows/new/addshow/', views.addshow),
     path('shows/<int:num>/', views.detail),
-    path('showss/<int:num>/edit', views.edit),
+    path('shows/<int:num>/edit', views.edit),
     path('shows/<int:num>/destroy', views.destroy),
 ]
